cc9ef39834b7e5fb2b045201ed03253edd9fa766
andresplatarrueda87 <andres.platarrueda@kikes.com.co>
2026-08-13 16:09:43 -0500

    Add files via upload

--- serve.py
+++ serve.py
@@ -1,32 +1,19 @@
 import http.server
 import os
-import urllib.parse
 import sys
-import mimetypes
+import socketserver
 
-web_dir = os.path.dirname(os.path.realpath(__file__))
+web_dir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(web_dir)
 port = 8088
 
-mimetypes.add_type('text/css', '.css')
-mimetypes.add_type('application/javascript', '.js')
-mimetypes.add_type('application/json', '.json')
-mimetypes.add_type('image/png', '.png')
-mimetypes.add_type('image/svg+xml', '.svg')
-
 class PreopHandler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=web_dir, **kwargs)
-
-    def translate_path(self, path):
-        path = urllib.parse.unquote(path)
-        path = path.split('?', 1)[0]
-        path = path.split('#', 1)[0]
-        return super().translate_path(path)
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-cache')
+        super().end_headers()
 
 if __name__ == '__main__':
-    http.server.ThreadingHTTPServer.allow_reuse_address = True
-    with http.server.ThreadingHTTPServer(("", port), PreopHandler) as httpd:
-        print(f"Serving KIKES PREOPERACIONAL at http://127.0.0.1:{port}")
-        sys.stdout.flush()
-        httpd.serve_forever()
+    socketserver.TCPServer.allow_reuse_address = True
+    server = http.server.HTTPServer(("", port), PreopHandler)
+    print(f"Servidor Kikes Preoperacional corriendo en http://127.0.0.1:{port}", flush=True)
+    server.serve_forever()
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kikes-preop-v51';
+const CACHE_NAME = 'kikes-preop-v55';
 const ASSETS = [
   './',
   './index.html',
